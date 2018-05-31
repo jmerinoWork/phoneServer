e86839337c035ded6622ef81ad0ffa4e3f76d872
Julen Merino Fernández <jmerino@ntsmobile.com>
2018-05-31 09:11:03 +0200

    fixed

--- pruebaLlamada.js
+++ pruebaLlamada.js
@@ -1,10 +1,10 @@
 "use strict";
 var shell = require('shelljs');
 
-async function doAction(){
-	await shell.exec('termux-telephony-call 629474997',async);
+function doAction(){
+	shell.exec('termux-telephony-call 629474997');
 }
-(async function(){
-	await doAction();
+(()=>{
+	doAction();
 	console.log("executed");
 })()
